4c6d96212459ec3cc31732e8e66e11bac953b3df
1volkman1 <igarodin@gmail.com>
2014-07-28 17:56:46 +0300

    type and object

---
+++ Meliage Calculator/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meliage_Calculator
+{
+    public partial class Form1 : Form
+    {
+        int startMileage;
+        int endingMileage;
+        double milesTraveling;
+        double reimburseRates = 0.39;
+        double amountOwed;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startMileage = (int) numericUpDown1.Value;
+            endingMileage = (int) numericUpDown2.Value;
+
+            if (startMileage > endingMileage)
+            {
+                MessageBox.Show(" Начальный пробег не может превышать конечный");
+            }
+            else
+            {
+                milesTraveling = endingMileage -= startMileage;
+                amountOwed = milesTraveling *= reimburseRates;
+                label4.Text = "$" + amountOwed;
+            }
+
+;        }
+    }
+}
