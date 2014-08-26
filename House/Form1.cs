b8908494ec9e240a81a8b0ef450b2b1356fdd9f8
1volkman1 <igarodin@gmail.com>
2014-08-26 19:33:32 +0300

    House.csproj learn inheritance and encapsulation

---
+++ House/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace House
+{
+    public partial class Form1 : Form
+    {
+        Location currentLocation;
+
+        RoomWithDoor livingRoom;
+        Room diningRoom;
+        RoomWithDoor kitchen;
+
+        OutsideWithDoor frontYard;
+        OutsideWithDoor backYard;
+        Outside garden;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateObjects();
+            MoveToANewLocation(livingRoom);
+        }
+
+        private void CreateObjects()
+        {
+            livingRoom = new RoomWithDoor("Гостиная", "старинный ковер", "дубовая дверь с латунной ручкой");
+            diningRoom = new Room("Столовая", "хрустальная люстра");
+            kitchen = new RoomWithDoor("Кухня", "плита из нержавеющей стали", "сетчатая дверь");
+
+            frontYard = new OutsideWithDoor("лужайка", false, "сетчатая дверь");
+            backYard = new OutsideWithDoor ("Задний двор", true, "сетчатая дверь");
+            garden = new Outside("Сад", false);
+
+            diningRoom.Exits = new Location[] { livingRoom, kitchen };
+            livingRoom.Exits = new Location[] { diningRoom };
+            kitchen.Exits = new Location[] { diningRoom };
+            frontYard.Exits = new Location[] { backYard, garden };
+            backYard.Exits = new Location[] { frontYard,garden};
+            garden.Exits = new Location[] { backYard,frontYard};
+
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
+
+            kitchen.DoorLocation = backYard;
+            backYard.DoorLocation = kitchen;
+
+        }
+
+        private void MoveToANewLocation(Location newLocation)
+        {
+            currentLocation = newLocation;
+
+            exits.Items.Clear();
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+                exits.Items.Add(currentLocation.Exits[i].Name);
+            exits.SelectedIndex = 0;
+
+            description.Text = currentLocation.Description;
+
+            if (currentLocation is IHasExteriorDoor)
+                goThrougthTheDoor.Visible = true;
+            else
+                goThrougthTheDoor.Visible = false;
+        }
+
+        private void goHere_Click(object sender, EventArgs e)
+        {
+            MoveToANewLocation(currentLocation.Exits[exits.SelectedIndex]);
+        }
+
+        private void goThrougthTheDoor_Click(object sender, EventArgs e)
+        {
+            IHasExteriorDoor hasDoor = currentLocation as IHasExteriorDoor;
+            MoveToANewLocation(hasDoor.DoorLocation);
+        }
+    }
+}
---
+++ House/Location.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+     abstract class Location
+    {
+         public Location(string name)
+         {
+             this.name = name;
+         }
+
+         public Location[] Exits;
+
+         private string name;
+         public string Name
+         {
+             get { return name;}
+         }
+
+         public virtual string Description
+         {
+             get
+             {
+                 string description = "Вы находитесь в " + name + " . Вы видите двери, ведущие в: ";
+                 for (int i = 0; i < Exits.Length; i++)
+                 {
+                     description += " " + Exits[i].Name;
+                     if (i != Exits.Length - 1)
+                         description += ",";
+                 }
+                 description += ".";
+                 return description;
+             }
+         }
+    }
+}
